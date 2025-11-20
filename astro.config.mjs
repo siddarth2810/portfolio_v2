693a16e77a45719f13c0c29ad30546b2fa19f1a1
Siddarth <110726331+siddarth2810@users.noreply.github.com>
2025-11-20 22:20:36 +0530

    update base path configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
-
 import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), icon()],
   site: 'https://siddarth2810.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/portfolio_v2' : '/',
+  base: '/',  
 });
