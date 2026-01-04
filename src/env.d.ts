fe65903d764f84ff7395ecda9dfddc518ac35d19
Siddarth Gundu <siddarthg0910@gmail.com>
2026-01-04 23:40:02 +0530

    2026 revamp

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+	readonly BASE_URL: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
+
+declare module "astro-font" {
+	export const AstroFont: any;
+}
